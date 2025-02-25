026258ad09a6b0250de7484b4a4835e45971016b
arailym-yerzhan <arailym.erzhan06@gmail.com>
2025-02-26 01:45:33 +0800

    generators.py

--- Lab4/tasks/generators.py
+++ Lab4/tasks/generators.py
@@ -1,6 +1,8 @@
 def square_generator(N):
     for i in range(N + 1):
         yield i ** 2
+    
+
 
 def even_numbers(n):
     for i in range(n + 1):
@@ -19,40 +21,55 @@ def squares(a, b):
 def countdown(n):
     for i in range(n, -1, -1):
         yield i
+def display_menu():
+    print("1. Generate squares from 0 to N")
+    print("2. Generate even numbers from 0 to N")
+    print("3. Generate numbers divisible by 3 and 4 from 0 to N")
+    print("4. Generate squares between a and b")
+    print("5. Countdown from N")
+    print("6. Exit")
 
-def main():
+def menu():
     while True:
-        print("\nChoose an option:")
-        print("1. Generate squares up to N")
-        print("2. Print even numbers up to n")
-        print("3. Numbers divisible by 3 and 4 up to n")
-        print("4. Squares from a to b")
-        print("5. Countdown from n to 0")
-        print("6. Exit")
-        
-        choice = input("Enter your choice: ")
-        
-        if choice == "1":
-            N = int(input("Enter N: "))
-            print(list(square_generator(N)))
-        elif choice == "2":
-            n = int(input("Enter n: "))
-            print(", ".join(map(str, even_numbers(n))))
-        elif choice == "3":
-            n = int(input("Enter n: "))
-            print(list(divisible_by_3_and_4(n)))
-        elif choice == "4":
-            a = int(input("Enter a: "))
-            b = int(input("Enter b: "))
-            print(list(squares(a, b)))
-        elif choice == "5":
-            n = int(input("Enter n: "))
-            print(list(countdown(n)))
-        elif choice == "6":
-            print("Exiting...")
+        display_menu()
+        choice = input("Enter your choice (1-6): ")
+
+        if choice == '1':
+            N = int(input("Enter the value of N: "))
+            print("Squares from 0 to", N)
+            for square in square_generator(N):
+                print(square)
+
+        elif choice == '2':
+            n = int(input("Enter the value of N: "))
+            print("Even numbers from 0 to", n)
+            for number in even_numbers(n):
+                print(number)
+
+        elif choice == '3':
+            n = int(input("Enter the value of N: "))
+            print("Numbers divisible by 3 and 4 from 0 to", n)
+            for number in divisible_by_3_and_4(n):
+                print(number)
+
+        elif choice == '4':
+            a = int(input("Enter the value of a: "))
+            b = int(input("Enter the value of b: "))
+            print("Squares from", a, "to", b)
+            for square in squares(a, b):
+                print(square)
+
+        elif choice == '5':
+            n = int(input("Enter the value of N: "))
+            print("Countdown from", n)
+            for count in countdown(n):
+                print(count)
+
+        elif choice == '6':
+            print("Exiting the program. Goodbye!")
             break
+
         else:
             print("Invalid choice. Please try again.")
 
-if __name__ == "__main__":
-    main()
+menu()
