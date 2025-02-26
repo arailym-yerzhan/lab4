6cf0e766b9112eb3e8137f52f0144c4993a899ee
arailym-yerzhan <arailym.erzhan06@gmail.com>
2025-02-26 15:46:09 +0800

    parsing.py

--- Lab4/tasks/parsing.py
+++ Lab4/tasks/parsing.py
@@ -1,16 +1,14 @@
 import json
 
-# Load the JSON data from file
 with open("Lab4/tasks/sample-data.json") as file:
     data = json.load(file)
 
-# Print the header
 print("Interface Status")
 print("=" * 80)
 print(f"{'DN':<50} {'Description':<20} {'Speed':<8} {'MTU':<6}")
 print("-" * 80)
 
-# Extract and print the relevant information
+
 for item in data["imdata"]:
     attributes = item["l1PhysIf"]["attributes"]
     dn = attributes["dn"]
